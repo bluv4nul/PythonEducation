bb3ff4e253d25d70aafc1539a36d802b33a1f0a1
Andrew <andrewkafrolov76@gmail.com>
2025-10-05 13:39:54 +0300

    mistakes

--- lab_1/SumOfTwo.py
+++ lab_1/SumOfTwo.py
@@ -2,15 +2,5 @@ def findTarget(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i]+nums[j] == target:
-                return i, j
-    return 0, 0
-
-nums = [2,7,11,15]
-target = 9
-
-
-i1, i2 = findTarget(nums, target)
-if i1 == i2 == 0:
-    print("Нет подходящих чисел")
-else:
-    print("i1 =", i1, " i2 =", i2)
+                return [i, j]
+    return [0, 0]
